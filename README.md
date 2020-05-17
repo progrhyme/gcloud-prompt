4ae734a7597bbf52051d9aed6d5b95b43e0c268f
progrhyme <progrhyme@gmail.com>
2020-05-17 15:56:29 +0900

    1st implementation

---
+++ README.md
@@ -0,0 +1,61 @@
+# gcloud-prompt
+
+Fetch gcloud client configuration information to show on shell prompt.
+
+Currently, Bash & Zsh are supported.
+
+# Usage
+
+Zsh:
+
+```sh
+. path/to/gcloud-prompt/gcloud-prompt.sh
+# Example prompt setting
+PROMPT="%F{cyan}[\$(gcloud_prompt)]%f %n@%m%# "
+```
+
+Bash:
+
+```sh
+. path/to/gcloud-prompt/gcloud-prompt.sh
+# Example prompt setting
+PS1="\[\e[0;36m\]<\$(gcloud_prompt)>\[\e[m\]\n\h:\W \u\$ "
+```
+
+Execute `toggle_gcloud_prompt` function to hide/show gcloud information.
+
+# Examples
+
+Here are some output examples.
+
+```sh
+# Default setting & default configuration
+$ gcloud_prompt
+default|bob@example.com,my-project
+
+# GCLOUD_PROMPT_CONFIG_KEYS=(core.project compute.region)
+$ gcloud_prompt
+default|my-project,asia-northeast1
+
+# GCLOUD_PROMPT_SHOW_CONFIG_PARAMS=no
+$ gcloud_prompt
+default
+```
+
+# Configuration
+
+You can change below variables to customize the prompt output.
+
+ Variable | Default | Type | Description
+----------|:-------:|:----:|---------------
+ GCLOUD_PROMPT_ENABLED | on | String | If this variable is not set, `gcloud_prompt` prints nothing
+ GCLOUD_PROMPT_SHOW_CONFIG_PARAMS | yes | String | If this variable doesn't equal to "yes", only current active configuration shows on prompt
+ GCLOUD_PROMPT_CONFIG_KEYS | `(core.account core.project)` | Array | When `GCLOUD_PROMPT_SHOW_CONFIG_PARAMS` is set to `yes`, these properties appears on prompt
+ GCLOUD_PROMPT_SEPARATER | `\|` | String | Delimiter between active configuration and config properties
+
+For available properties to set in `GCLOUD_PROMPT_CONFIG_KEYS`, refer to https://cloud.google.com/sdk/gcloud/reference/config/get-value
+
+# Dependencies
+
+This script depends on the current implementation of Cloud SDK; such as configuration
+directory structure, configuration data structure etc.
---
+++ gcloud-prompt.sh
@@ -0,0 +1,74 @@
+# bash/zsh
+
+if ! command -v gcloud &>/dev/null; then
+  "[ERROR] Command \"gcloud\" is missing! Can't load gcloud-prompt.sh"
+  return
+fi
+
+GCLOUD_PROMPT_ENABLED=on
+GCLOUD_PROMPT_SHOW_CONFIG_PARAMS=${GCLOUD_PROMPT_SHOW_CONFIG_PARAMS:-yes}
+if [[ -z "${GCLOUD_PROMPT_CONFIG_KEYS:-}" ]]; then
+  GCLOUD_PROMPT_CONFIG_KEYS=(core.account core.project)
+fi
+GCLOUD_PROMPT_SEPARATER=${GCLOUD_PROMPT_SEPARATER:-'|'}
+
+GCLOUD_PROMPT_ROOT=${GCLOUD_PROMPT_ROOT:-$HOME/.gcloud_prompt}
+GCLOUD_PROMPT_CACHE_DIR="$GCLOUD_PROMPT_ROOT/cache"
+
+# initialize cache directory
+[[ -d $GCLOUD_PROMPT_CACHE_DIR ]] && rm -rf $GCLOUD_PROMPT_CACHE_DIR
+mkdir -p $GCLOUD_PROMPT_CACHE_DIR
+
+if [[ -z "${GCLOUD_PROMPT_SDK_CONFIG_DIR:-}" ]]; then
+  GCLOUD_PROMPT_SDK_CONFIG_DIR=$(gcloud info --format='value(config.paths.global_config_dir)')
+fi
+
+# prompt builder function
+gcloud_prompt() {
+  if [[ -z "${GCLOUD_PROMPT_ENABLED:-}" ]]; then
+    return
+  fi
+
+  if [[ "$GCLOUD_PROMPT_SHOW_CONFIG_PARAMS" = "yes" ]]; then
+    printf '%s%s%s' \
+      $(_gcloud_prompt_sdk_active_config) \
+      $GCLOUD_PROMPT_SEPARATER \
+      $(_gcloud_prompt_config_values)
+  else
+    printf '%s' $(_gcloud_prompt_sdk_active_config)
+  fi
+}
+
+# toggle show/hide prompt
+toggle_gcloud_prompt() {
+  if [[ -z "${GCLOUD_PROMPT_ENABLED:-}" ]]; then
+    GCLOUD_PROMPT_ENABLED=on
+  else
+    GCLOUD_PROMPT_ENABLED=
+  fi
+}
+
+# fetch activated configuration
+_gcloud_prompt_sdk_active_config() {
+  if [[ -n "${CLOUDSDK_ACTIVE_CONFIG_NAME:-}" ]]; then
+    echo $CLOUDSDK_ACTIVE_CONFIG_NAME
+  else
+    cat "${GCLOUD_PROMPT_SDK_CONFIG_DIR}/active_config"
+  fi
+}
+
+# fetch config values
+_gcloud_prompt_config_values() {
+  local cfgn=$(_gcloud_prompt_sdk_active_config)
+  local cfgn_path="${GCLOUD_PROMPT_SDK_CONFIG_DIR}/configurations/config_${cfgn}"
+  local cache_path="${GCLOUD_PROMPT_CACHE_DIR}/config_${cfgn}.values"
+
+  if [[ ! -r "$cache_path" || "$cfgn_path" -nt "$cache_path" ]]; then
+    # create cache file
+    # join arrays into comma-separated string
+    local keys="$(IFS=,; echo "${GCLOUD_PROMPT_CONFIG_KEYS[*]}")"
+    gcloud config list --format="csv[no-heading]($keys)" > $cache_path
+  fi
+
+  cat $cache_path
+}
